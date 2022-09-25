e27647534f1a8ee8fdac2d24620044eaed19a9e7
Yaknan John Gambo <yaknanjohn@gmail.com>
2022-09-25 01:34:43 +0000

    Added web pages

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 #Importing the Flask class libraries
-from flask import Flask
+from flask import Flask, render_template
 
 #Creating an app instance
 app = Flask(__name__)
 
+
 #Creating a route
 @app.route("/")
-
 #Defining a function to be called
 def home():
-    return "Chapel Home Page"
+    return render_template('index.html')
+
+#Creating a route
+@app.route("/about")
+#Defining a function to be called
+def home():
+    return render_template('about.html')
 
 print("__name__")
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
